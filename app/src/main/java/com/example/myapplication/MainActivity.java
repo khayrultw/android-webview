b987c89ca242b9ecae4c529a7058f1c5e699281a
beta-ori <orion.beta.ori@gmail.com>
2019-07-20 15:00:28 +0600

    This is an android app with browsing functionality.

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    WebView browser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        browser = findViewById(R.id.web);
+        browser.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.setWebViewClient(new MyWebViewClient());
+        browser.loadUrl("https://www.google.com");
+    }
+
+    private class MyWebViewClient extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(browser.canGoBack())
+            browser.goBack();
+    }
+}
+
+
